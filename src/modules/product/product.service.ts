7b074da7c01336241e357995c784cbf79ebe72b4
Rony de Lucca <ronylucca@gmail.com>
2022-12-23 14:25:49 -0300

    added logs, comments and query updates

--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -1,4 +1,10 @@
-import { forwardRef, Inject, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  forwardRef,
+  Inject,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { Product } from '@prisma/client';
 import { BlockchainService } from 'src/modules/blockchain/blockchain.service';
 import { PrismaService } from 'src/prisma.service';
@@ -6,6 +12,8 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { randomUUID } from 'node:crypto';
 
 export class ProductService {
+  protected logger = new Logger(this.constructor.name);
+
   constructor(
     @Inject(forwardRef(() => BlockchainService))
     private readonly blockchainService: BlockchainService,
@@ -16,17 +24,33 @@ export class ProductService {
     return this.prisma.product.findMany();
   }
 
+  /**
+   * @description request Product creation on blockchain and persists the data on DB
+   * @param CreateProductDto
+   * @returns {Product}
+   * @emits BadRequestException
+   */
   async createProduct(dto: CreateProductDto): Promise<Product> {
-    dto.tokenId = await this.blockchainService.createAuctionProductBlockchain(
-      dto.name,
-      dto.seller,
-    );
-    return this.prisma.product.create({
-      data: {
-        id: randomUUID(),
-        ...dto,
-      },
-    });
+    try {
+      dto.tokenId = await this.blockchainService.createAuctionProductBlockchain(
+        dto.name,
+        dto.seller,
+      );
+      return this.prisma.product.create({
+        data: {
+          id: randomUUID(),
+          ...dto,
+        },
+      });
+    } catch (error) {
+      this.logger.error(
+        'Could not mint NFT product token ${error.message}',
+        error.stack,
+      );
+      throw new BadRequestException(
+        'Product could not be crated or minted. Check data request',
+      );
+    }
   }
 
   async getProductsById(id: string): Promise<Product> {
@@ -34,6 +58,7 @@ export class ProductService {
       where: {
         id,
       },
+      include: { Auction: true },
     });
     if (!product) {
       throw new NotFoundException();
